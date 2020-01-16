e83e508c9221301ed9cf7a4da2216db9cd7b0998
Héctor Naranjo <hectoritiin@hotmail.com>
2020-01-16 17:26:01 +0100

    arreglo ingred

--- resources/views/apartados/ingredients.blade.php
+++ resources/views/apartados/ingredients.blade.php
@@ -42,34 +42,34 @@
             </td>
         </tr>
         @endforeach
-        @foreach ($ingredients as $ingr)
-          <!-- Modal -->
-          <div class="modal fade" id="{{ $ingr->name }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Confirmacio d'eliminació</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  Estas segur que vols eliminar aquesta comanda?
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Tornar</button>
-                  <form action="{{action('IngredientController@deleteDash', $ingr->id)}}" method="POST" style="display:inline">
-                      {{ method_field('PUT') }}
-                      {{ csrf_field() }}
-                      <button type="submit" class="btn btn-primary">Eliminar</button>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        @endforeach
+
     </tbody>
 </table>
-
+@foreach ($ingredients as $ingr)
+  <!-- Modal -->
+  <div class="modal fade" id="{{ $ingr->name }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Confirmacio d'eliminació</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          Estas segur que vols eliminar aquesta comanda?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Tornar</button>
+          <form action="{{action('IngredientController@deleteDash', $ingr->id)}}" method="POST" style="display:inline">
+              {{ method_field('PUT') }}
+              {{ csrf_field() }}
+              <button type="submit" class="btn btn-primary">Eliminar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+@endforeach
 {{ $ingredients->links() }}
 @endsection
