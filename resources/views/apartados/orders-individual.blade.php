756320506e2939dd715928526e4d77f431278127
Dylan <dylanhurtos@gmail.com>
2020-01-16 16:04:20 +0100

    Modificacions Orders individuals

--- resources/views/apartados/orders-individual.blade.php
+++ resources/views/apartados/orders-individual.blade.php
@@ -8,19 +8,23 @@
   <div class="row ">
     <div class="col-md-5 details mt-4 text-center align-self-center">
         <h1 class="mb-0">NUM PEDIDO: {{ $order->id }}</h1><br>
+        <h1>ENTREGAT:
+            @if(($order->finished==0))
+                No
+            @else
+                Sí
+            @endif
+        </h1>
 
     </div>
     <div class="col-md-4 details mt-2 align-self-center ">
       <blockquote class="mb-0">
         <h5 class="mb-0">{{ $order->user->name }}</h5>
-        <small><cite title="Source Title">#ID Usuario:{{ $order->user->id }}<i class="icon-map-marker"></i></cite></small>
+        <small><cite title="Source Title">{{ $order->user->email }}<i class="icon-map-marker"></i></cite></small>
       </blockquote>
-      <p>
-        {{ $order->user->email }}<br>
-      </p>
     </div>
     <div class="col-md-3 img">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvzOpl3-kqfNbPcA_u_qEZcSuvu5Je4Ce_FkTMMjxhB-J1wWin-Q"  alt="" class="img-rounded">
+      <img src="{{ asset('black') }}/img/{{ $order->user->avatar }}"  alt="" class="img-rounded">
     </div>
   </div>
 </div>
@@ -32,7 +36,7 @@
             <th>PRODUCTO</th>
             <th>CANTIDAD</th>
             <th>PRECIO</th>
-            <th>OPCIONES</th>
+            <th>ELIMINAR</th>
         </tr>
     </thead>
     <tbody class="text-center">
@@ -43,8 +47,8 @@
             <td>{{ $prod->pivot->Quantity }}</td>
             <td>{{ $prod->price }}&euro;</td>
             <td class="td-actions text-center">
-                <button type="button" rel="tooltip" class="btn btn-success btn-link btn-icon btn-sm">
-                    <i class="tim-icons icon-settings"></i>
+                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-icon btn-sm">
+                    <i class="tim-icons icon-simple-remove"></i>
                 </button>
             </td>
         </tr>
@@ -54,13 +58,42 @@
   <div class="container mb-5 text-right">
   <div class="row ">
     <div class="col-md-12 col-lg-2 details mt-4 text-center mr-0 mr-xs-5" >
-            <button type="submit" class="btn btn-primary">Entregat</button>
+            @if(($order->finished==0))
+                <button type="button" class="btn btn-primary">Entregar</button>
+            @else
+                <button type="button" class="btn btn-primary">Cancelar</button>
+            @endif
     </div>
     <div class=" col-md-12 col-lg-2 details mt-4 text-center">
-            <button type="submit" class="btn btn-primary">Eliminar</button>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Eliminar</button>
     </div>
-    <div class="col-md-12 col-sm-7 ml-sm-2 ml-md-0 col-lg-4 details mt-4">
-        <h1>TOTAL: </h1>
+    <div class="col-md-12 col-xs-5 col-sm-7 ml-sm-2 ml-md-0 col-lg-4 details mt-4">
+        <h1 id="preutotal">TOTAL:  {{ $price }} &euro;</h1>
+        <script>
+            
+        </script>
+    </div>
+  </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Confirmacio d'eliminació</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Estas segur que vols eliminar aquesta comanda?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tornar</button>
+        <button type="button" class="btn btn-primary">Eliminar</button>
+      </div>
     </div>
   </div>
 </div>
