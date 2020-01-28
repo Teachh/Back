457f69573998d8a6fdc9c7d4adb1aff13b6f5097
Héctor Naranjo <hectoritiin@hotmail.com>
2020-01-28 15:14:04 +0100

    Cambio de orden

--- public/black/js/theme.js
+++ public/black/js/theme.js
@@ -451,8 +451,7 @@ demo = {
         display: false
       },
       data: {
-        labels: [productosOrdeandosNombre[0],productosOrdeandosNombre[1] ,productosOrdeandosNombre[2] ,productosOrdeandosNombre[3] ,productosOrdeandosNombre[4] ],
-        //labels: [productosOrdeandos[productosOrdeandosNombre[0]].length, productosOrdeandos[productosOrdeandosNombre[1]].length, productosOrdeandos[productosOrdeandosNombre[2]].length, productosOrdeandos[productosOrdeandosNombre[3]].length, productosOrdeandos[productosOrdeandosNombre[4]].length],
+        labels: [productosOrdeandosNombre[0],productosOrdeandosNombre[1] ,productosOrdeandosNombre[2] ,productosOrdeandosNombre[3] ,productosOrdeandosNombre[4]],
         datasets: [{
           label: "Ingredients",
           fill: true,
@@ -463,6 +462,7 @@ demo = {
           borderDash: [],
           borderDashOffset: 0.0,
           data: [productosOrdeandos[productosOrdeandosNombre[0]].length, productosOrdeandos[productosOrdeandosNombre[1]].length, productosOrdeandos[productosOrdeandosNombre[2]].length, 4, 5],
+          //labels: [productosOrdeandos[productosOrdeandosNombre[0]].length, productosOrdeandos[productosOrdeandosNombre[1]].length, productosOrdeandos[productosOrdeandosNombre[2]].length, productosOrdeandos[productosOrdeandosNombre[3]].length, productosOrdeandos[productosOrdeandosNombre[4]].length],
         }]
       },
       options: gradientBarChartConfiguration
